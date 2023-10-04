d10e210db38cc4d341e795f73ae517f18d70d87b
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-04 17:35:55 +0530

    Update api.js

--- hotel-listing-frontend/src/services/api.js
+++ hotel-listing-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/api'; // Specify your API base URL here
+const baseURL = 'https://rentme-8yha.onrender.com/api'; 
 
 const api = axios.create({
   baseURL: baseURL,
