80b7b98f6bb9ac37a5f20becd28b4f1f6a959097
geek-sanjay <workrework.sanjay@gmail.com>
2023-10-04 17:28:59 +0530

    Initial commit

---
+++ hotel-listing-frontend/src/components/PropertyDetails.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../services/api';
+import '../styles/PropertyDetails.css';
+
+const PropertyDetails = () => {
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+
+  useEffect(() => {
+    const fetchProperty = async () => {
+      try {
+        const response = await api.get(`/properties/${id}`);
+        setProperty(response.data);
+      } catch (error) {
+        console.error('Error fetching property details:', error);
+      }
+    };
+
+    fetchProperty();
+  }, [id]);
+
+  if (!property) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="property-details-container">
+      <div className="property-details">
+        <h2>{property.name}</h2>
+        <p>City: {property.city}</p>
+        <p>Bedrooms: {property.bedrooms}</p>
+        <p>Bathrooms: {property.bathrooms}</p>
+        <p>Price: ${property.price}</p>
+        <img src={property.imageUrl} alt={property.name} className="property-image" />
+        {/* ...other property details */}
+      </div>
+    </div>
+  );
+};
+
+export default PropertyDetails;
